ff46c8ba51ef64e91386b983430324bfd9ea6d18
pgg <93646592+pggoing@users.noreply.github.com>
2021-11-03 20:55:01 +0800

    和差积商

---
+++ 和差积商.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+	int x,y,he,cha,ji,shang;
+	scanf("%d",&x);
+	scanf("%d",&y);
+	he=x+y;
+	cha=x-y;
+	ji=x*y;
+	shang=x/y;
+	printf("%d  ",he);
+	printf("%d  ",cha);
+	printf("%d  ",ji);
+	printf("%d  ",shang);
+	return 0;
+}
